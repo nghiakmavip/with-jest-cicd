d596c087aa5e34fba422f8f83eb8f53a2e5f51f9
nghiakmavip <trannnghia155@gmail.com>
2026-02-24 08:40:03 +0700

    init project

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` };
+}
+
+export default function Page({ params }: Params) {
+  return <><h1>Slug: {params.slug}</h1><p>TranNghia</p>
+  <p>Welcome to HCM</p>
+  <p>Welcome HANOi</p></>;
+}
